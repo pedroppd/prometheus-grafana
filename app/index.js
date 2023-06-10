8562d144c121ac49f8412ec3f45f5f6eb63edd2f
pedroppd <pedro_pdantas@hotmail.com>
2023-06-10 14:43:07 -0300

    separating the constants and adding new metrics for prometheus

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const {PORT} = require('./constants/constants');
+const prom = require('prom-client');
 const app = express();
-const PORT = 8080;
 
-app.listen(() => {
+const counter = new prom.Counter({
+    name: 'metric_name',
+    help: 'metric_help',
+  });
+  counter.inc();
+  counter.inc(10);
+
+
+app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
 
